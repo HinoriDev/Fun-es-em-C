48ce57f3544ffe982cfaac69189edc8f3eadd3ae
Guilherme Hinori <gui-benetti@hotmail.com>
2025-10-17 12:28:58 +0000

    funçoes

---
+++ funcoes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void hello() {
+    printf("Olá\n");
+}
+int soma(int num_a, int num_b){
+    return num_a + num_b;
+}
+
+int subtracao_positivo(int num_a, int num_b){
+    int resultado;
+    if(num_a > num_b){
+        resultado = num_a - num_b;
+    } else{
+        resultado = num_b - num_a;
+    }
+    return resultado;
+}
+
+int main() { //Função main, é o ponto de entrada da aplicação
+    hello();
+
+    printf("%d\n", soma(4, 5));
+    printf("%d\n", subtracao_positivo(10, 5));
+    printf("%d\n", subtracao_positivo(2, 4));
+
+    return 0;
+}
